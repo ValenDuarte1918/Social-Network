3d0696de30a4985bf11f127988ebc96cd7acc1f7
ValenDuarte1918 <valentinduarte1@hotmail.com>
2024-01-07 00:25:14 -0300

    Mejora en la navegabilidad

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
     return <header className="w-full">
     <nav className="w-full bg-blue-500 text-white p-2 mb-2">
-    <Link href="/explore">
+    <Link href="/">
         <div className="px-4 py-1">
             LOGO
         </div>
